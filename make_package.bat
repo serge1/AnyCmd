708329923252fac6c2f642d25e5fb5dca2d9fdde
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2013-02-06 20:32:12 +0200

    style.css file was incorporated into index.htm. No need to distribute it

--- make_package.bat
+++ make_package.bat
@@ -1,3 +1,3 @@
 msbuild anycmd.vcxproj /t:Rebuild /p:project="anycmd" /p:platform="Win32" /p:configuration="Release"
 msbuild anycmd.vcxproj /t:Rebuild /p:project="anycmd" /p:platform="x64" /p:configuration="Release"
-cscript zip.vbs wlx_anycmd_1.1.zip Release\anycmd.wlx x64\Release\anycmd.wlx64 pluginst.inf doc\index.htm doc\style.css
+cscript zip.vbs wlx_anycmd_1.1.zip Release\anycmd.wlx x64\Release\anycmd.wlx64 pluginst.inf doc\index.htm
