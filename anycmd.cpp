b43e879cdd98af777975464c0977656354e4ea34
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2013-05-13 20:07:37 +0300

    Remove tabs and fix CR/LF

--- anycmd.cpp
+++ anycmd.cpp
@@ -25,7 +25,7 @@ THE SOFTWARE.
 */
 
 #define _CRT_SECURE_NO_WARNINGS
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN        // Exclude rarely-used stuff from Windows headers
 
 #include <windows.h>
 #include <stdlib.h>
